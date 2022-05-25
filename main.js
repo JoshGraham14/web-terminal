f75899bf9085b02d2344243dbf7d39413ba39552
Josh Graham <joshgraham14@gmail.com>
2022-05-24 21:12:14 -0400

    :sparkles: Commands are now inserted above the prompt in the DOM

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var termPrompt = document.getElementById('prompt')
+var commandHistory = []
 
 termPrompt.addEventListener('keypress', e => {
 	if (e.key === 'Enter') {
@@ -8,5 +9,22 @@ termPrompt.addEventListener('keypress', e => {
 })
 
 const handleSubmit = command => {
-	console.log(command)
+	commandHistory.push(command)
+	const lineSpan = document.createElement('span')
+	const newLinePrompt = document.createElement('p')
+	const newLineCommand = document.createElement('p')
+	const promptText = document.createTextNode('user@josh $ ')
+	const commandText = document.createTextNode(command)
+	newLinePrompt.appendChild(promptText)
+	newLinePrompt.classList.add('prompt-path')
+	newLineCommand.appendChild(commandText)
+	newLineCommand.classList.add('prompt-input')
+	lineSpan.appendChild(newLinePrompt)
+	lineSpan.appendChild(newLineCommand)
+	lineSpan.classList.add('prompt-span')
+	const termWindow = document.getElementById('window')
+	termWindow.insertBefore(
+		lineSpan,
+		termWindow.children[termWindow.children.length - 1]
+	)
 }
